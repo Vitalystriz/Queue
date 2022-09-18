6357b991d9f5b3a1ff62ccacb17d7df7df877333
German Strizhevsky <vitalstri@ya.ru>
2022-09-18 22:02:43 +0300

    create ne branch

---
+++ Studying_in_israeli_school/Studying_in_israeli_school/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Studying_in_israeli_school;
+
+Person tom = new Person("op", 8);
+Console.WriteLine(tom.Name);
+tom.Print();
---
+++ Studying_in_israeli_school/Studying_in_israeli_school/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studying_in_israeli_school
+{
+  
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age) { this.Name = name; this.Age = age; }
+      
+        public void Print()
+        {
+            Console.WriteLine(this.Name);
+        }
+
+    }
+
+        
+   
+    
+
+
+}
